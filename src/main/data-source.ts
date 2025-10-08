a7bed11a220375578056a77dc357115d76998aec
KJW <cmcncb13624@gmail.com>
2025-10-09 00:40:39 +0900

    chore. db 기본 경로 변경

--- src/main/data-source.ts
+++ src/main/data-source.ts
@@ -5,13 +5,27 @@ import { KindEntity } from '../entity/kindEntity'
 import { ItemEntity, ItemAndCategory, ItemAndKind } from '../entity/itemEntity'
 import { ImageMappingEntity } from '../entity/imageMappingEntity'
 import { FileRefEntity } from '../entity/fileRefEntity'
+import path from 'node:path'
+import { app } from 'electron'
+
+const dbPath = path.join(app.getPath('userData'), 'database.sqlite')
+
+console.log(dbPath)
 
 export const AppDataSource = new DataSource({
   type: 'sqlite',
-  database: 'database.sqlite', // 로컬 DB 파일
+  database: dbPath, // 로컬 DB 파일
   synchronize: true, // 개발용: 엔티티 기반 테이블 자동 생성
   logging: true,
-  entities: [CategoryEntity, KindEntity, ItemEntity, ItemAndCategory, ItemAndKind, ImageMappingEntity, FileRefEntity],
+  entities: [
+    CategoryEntity,
+    KindEntity,
+    ItemEntity,
+    ItemAndCategory,
+    ItemAndKind,
+    ImageMappingEntity,
+    FileRefEntity
+  ],
   migrations: [],
   subscribers: []
 })
