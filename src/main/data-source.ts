68677977d70c5a3d5f2a6bcd70b6bfdbade5e55c
KJW <cmcncb13624@gmail.com>
2026-06-23 23:47:11 +0900

    chore. 초기 실행시 entity 생성을 위해서 DataSource에 synchronize를 true로 변경

--- src/main/data-source.ts
+++ src/main/data-source.ts
@@ -8,14 +8,12 @@ import { FileRefEntity } from '../entity/fileRefEntity'
 import path from 'node:path'
 import { app } from 'electron'
 
-import { is } from '@electron-toolkit/utils'
-
 const dbPath = path.join(app.getPath('userData'), 'database.sqlite')
 
 export const AppDataSource = new DataSource({
   type: 'sqlite',
   database: dbPath, // 로컬 DB 파일
-  synchronize: is.dev, // 개발용: 엔티티 기반 테이블 자동 생성 (상용에서는 false 권장)
+  synchronize: true,
   logging: true,
   entities: [
     CategoryEntity,
